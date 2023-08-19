1f028f25c85916a85ea8776e50b02fc4cff43dd7
nalinathukorala <nalinathukoralanalin@gmail.com>
2023-08-19 20:30:40 +0530

    chanage  port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const { json } = require('express');
 const express = require('express');
 
 const app = express();
+const PORT = 3000 || process.env.PORT;
 
 app.get('/', (req, res) => {
     res.json('hello world')
@@ -71,6 +72,6 @@ app.get('/name/:id', (req,res) =>{
    
 })
 
-app.listen(3000, () => (
+app.listen(PORT, () => (
     console.log('port open')
 ))
